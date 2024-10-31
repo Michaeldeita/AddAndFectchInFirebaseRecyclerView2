1b6a8e96cbe454e5f36ab51df56b18646eae0aaa
Alesandro Hobayan <alesandrosfboo@gmail.com>
2024-10-31 17:17:36 +0800

    feat: recyclerView added, working firebase

--- app/src/main/java/com/example/addandfectchinfirebaserecyclerview2/MainActivity.java
+++ app/src/main/java/com/example/addandfectchinfirebaserecyclerview2/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.addandfectchinfirebaserecyclerview2;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.addandfectchinfirebaserecyclerview2.productAdapter.ProductAdapter;
 import com.example.addandfectchinfirebaserecyclerview2.productModel.ProductModel;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,9 +21,13 @@ public class MainActivity extends AppCompatActivity {
 
     EditText id, name, category, price;
     Button add, list;
+    RecyclerView rvMain;
 
     List<ProductModel> productList = new ArrayList<>();
     ProductModel productObject;
+    ProductAdapter productAdapter;
+
+    FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         productObject = new ProductModel();
+        firestore = FirebaseFirestore.getInstance();
 
         id = findViewById(R.id.et_product_id);
         name = findViewById(R.id.et_product_name);
@@ -32,12 +43,17 @@ public class MainActivity extends AppCompatActivity {
         price = findViewById(R.id.et_product_price);
         add = findViewById(R.id.btn_product_add);
         list = findViewById(R.id.btn_product_list);
+        rvMain = findViewById(R.id.rv_main);
 
         //btn add functionality
         add.setOnClickListener(view -> toAdd());
 
         //btn list functionality
         list.setOnClickListener(view -> toList());
+
+
+
+
     }
 
     //add function
@@ -46,11 +62,29 @@ public class MainActivity extends AppCompatActivity {
         productObject.setName(name.getText().toString());
         productObject.setCategory(category.getText().toString());
         productObject.setPrice(Integer.parseInt(price.getText().toString()));
+
+        Log.d("MAIN", "sandro");
+        firestore.collection("products").add(productObject).addOnSuccessListener(document -> {
+            productList.add(productObject);
+
+        }).addOnFailureListener(e -> {
+            Log.d("MAIN", e.getMessage());
+        });
     }
 
     //list function
     public void toList() {
-
+        firestore.collection("products").get().addOnCompleteListener(v->{
+            if(v.isSuccessful()) {
+                for (QueryDocumentSnapshot document : v.getResult()) {
+                    productAdapter = new ProductAdapter(productList);
+                    rvMain.setAdapter(productAdapter);
+                    rvMain.setLayoutManager(new LinearLayoutManager(this));
+                }
+            }else{
+                Log.e("MAIN",v.getException().getMessage());
+            }
+        });
     }
 
 }
